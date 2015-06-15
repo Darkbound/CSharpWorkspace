b8e41894b5ae46e87d1f0f7902c520bea6b4655b
Darkbound <vachev.t@gmail.com>
2015-06-15 21:29:00 +0300

    dfdsfds
    dds

---
+++ OOPHomework/DefineClasses/DefineClasses/Armor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// Define a class Armor which holds the following information: damageBlock, type (cask, shield etc), class (same as above).
+namespace DefineClasses
+{
+
+    public enum ArmorType
+    {
+        Head,
+        Shoulders,
+        Chest,
+        Wrists,
+        Hands,
+        Belt,
+        Legs,
+        Feet
+    }
+
+    public class Armor : Items
+    {
+        public ArmorType ArmorType { get; set; }
+        public int Defense { get; set; }
+
+        public Armor()
+        {
+
+        }
+
+        public Armor(ArmorType type, ItemQuality quality, int defense)
+        {
+            this.ArmorType = type;
+            this.ItemQuality = quality;
+            this.Defense = defense;
+        }
+
+        public Armor(ArmorType type, ItemQuality quality, int defense, int stamina, int agility)
+        {
+            this.ArmorType = type;
+            this.ItemQuality = quality;
+            this.Defense = defense;
+            this.Stamina = stamina;
+            this.Agility = agility;
+        }
+    }
+}
---
+++ OOPHomework/DefineClasses/DefineClasses/Hero.cs
@@ -0,0 +1,96 @@
+﻿/// Define a class Hero that holds information about your character: name, health, level, mana, strength, agility, 
+/// intelligence, primaryAttribute (as an enumeration, one of the previous 3), isBlocking, isDead, class (Sorcerer, Fighter, Tank), 
+
+namespace DefineClasses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public enum PrimaryAttribute
+	{
+        Strength,
+        Agility,
+        Intelligence
+	}
+
+    public enum Class
+    {
+        Tank,
+        Fighter,
+        Sorcerer
+    }
+    
+    public class Hero
+    {
+        public PrimaryAttribute PrimaryAttribute { get; private set; }
+        public Class Class 
+        {
+            get
+            {
+                return Class;
+            }
+            set
+            {
+                if (Class == Class.Tank)
+                {
+                    PrimaryAttribute = PrimaryAttribute.Strength;
+                    IsBlocking = true;
+                }
+                else if (Class == Class.Fighter)
+                {
+                    PrimaryAttribute = PrimaryAttribute.Agility;
+                    IsBlocking = false;
+                    IsDodging = true;
+                }
+                else if (Class == Class.Sorcerer)
+                {
+                    PrimaryAttribute = PrimaryAttribute.Intelligence;
+                    IsBlocking = false;
+                    IsDodging = false;
+                }
+            }
+        }
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Level { get; set; }
+        public int Mana { get; set; }
+        public int Strength { get; set; }
+        public int Agility { get; set; }
+        public int Intelligence { get; set; }
+        public bool IsDead { get; set; }
+        public bool IsBlocking { get; private set; }
+        public bool IsDodging { get; private set; }
+
+        public Hero()
+        {
+
+        }
+
+        public Hero(Class heroClass, string name, int health)
+        {
+            this.Class = heroClass;
+            this.Name = name;
+            this.Health = health;
+        }
+
+        public Hero(Class heroClass, string name, int health, int level)
+        {
+            this.Class = heroClass;
+            this.Name = name;
+            this.Health = health;
+            this.Level = level;
+        }
+
+        public Hero(Class heroClass, string name, int health, int level, bool isDead)
+        {
+            this.Class = heroClass;
+            this.Name = name;
+            this.Health = health;
+            this.Level = level;
+            this.IsDead = isDead;
+        }
+    }
+}
