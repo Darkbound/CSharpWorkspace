445831af906375cc69674355068494f3edf9562e
Darkbound <vachev.t@gmail.com>
2015-06-16 00:21:01 +0300

    drgdfgdf asd

---
+++ OOPHomework/DefineClasses/DefineClasses/Initialize.cs
@@ -0,0 +1,23 @@
+﻿namespace DefineClasses
+{
+    public class Initialize
+    {
+        public static void Tank(Hero hero, string name)
+        {
+            hero.Name = name;
+            hero.Class = Class.Tank;
+        }
+
+        public static void Fighter(Hero hero, string name)
+        {
+            hero.Name = name;
+            hero.Class = Class.Fighter;
+        }
+
+        public static void Sorcerer(Hero hero, string name)
+        {
+            hero.Name = name;
+            hero.Class = Class.Sorcerer;
+        }
+    }
+}
