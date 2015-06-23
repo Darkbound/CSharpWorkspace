4ecbedfd02be0c5a5b76ae1b3c413ab005c85340
Darkbound <vachev.t@gmail.com>
2015-06-23 23:54:33 +0300

    gd
    dgdfgdf

---
+++ OOPHomework/2.3 CoolStuffInCsharp/2.3 CoolStuffInCsharp/EntryPoint.cs
@@ -0,0 +1,13 @@
+﻿namespace CoolStuffInCsharp
+{
+    using CoolStuffInCsharp.Extensions;
+    using System.Text;
+
+    class EntryPoint
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
---
+++ OOPHomework/2.3 CoolStuffInCsharp/2.3 CoolStuffInCsharp/Extensions/StringBuilderExtension.cs
@@ -0,0 +1,56 @@
+﻿namespace CoolStuffInCsharp.Extensions
+{
+    using System;
+    using System.Text;
+
+    public static class StringBuilderExtension
+    {
+        public static StringBuilder Substring(this StringBuilder sb, int startIndex)
+        {
+            StringBuilder substring = new StringBuilder();
+
+            try
+            {
+                if (startIndex >= sb.Length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                for (int i = startIndex; i < sb.Length; i++)
+                {
+                    substring.Append(sb[i]);
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+
+                Console.Write("startIndex is greater than the length of the string" +
+                    "\n{0}", e);
+            }
+            
+            return substring;
+        }
+
+        public static StringBuilder Substring(this StringBuilder sb, int startIndex, int length)
+        {
+            StringBuilder substring = new StringBuilder();
+
+            try
+            {
+                for (int i = startIndex; i < startIndex + length; i++)
+                {
+                        substring.Append(sb[i]);
+                }
+                return substring;
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.Write("startIndex is either greater than the length of the string" + 
+                    "\nor startIndex+length don't reffer to an element within the bounds of the string" + 
+                    "\n{0}", e);
+            }
+
+            return null;
+        }
+    }
+}
