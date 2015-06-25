9d559f1570ef519cb74f674ed952663c9fe6be91
Darkbound <vachev.t@gmail.com>
2015-06-26 02:31:56 +0300

    dd

---
+++ OOPHomework/CoolStuffInCSharp/Lambda/EntryPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lambda
+{
+    class EntryPoint
+    {
+        static void Main()
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 21};
+
+            var newArray = array.Where(n => ((n % 3 == 0) && (n % 7 == 0)))
+                .Select(n => n);
+
+            foreach (var item in newArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
